60bb84c61b1047a71244e1e61653cbeb6a35533e
Natalie Halawani <hala1224@gmail.com>
2018-11-19 15:39:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
-    })
+      event.preventDefault();
+    });
 }
 
-preventRefreshOnSubmit()
+preventRefreshOnSubmit();
+
+function retrieveEmployeeInformation() {
+    const input=document.querySelector('input');
+    return input.value.toString();
+}
+
+
+function addNewElementAsLi(){
+  let employeeName = retrieveEmployeeInformation()
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`);
+}
+
+function addNewLiOnClick() {
+  const main=document.getElementById('main');
+  main.addEventListener("click",
+  function (event) {
+    retrieveEmployeeInformation();
+    addNewElementAsLi();
+  });
+}
